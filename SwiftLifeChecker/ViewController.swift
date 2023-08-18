d9a4207880ffca84f155f07fb7b6d02862fe5d8a
RyotaFukumoto <f.ryota.828@gmail.com>
2023-08-18 23:22:08 +0900

    Initial Commit

---
+++ SwiftLifeChecker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftLifeChecker
+//
+//  Created by ryota on 2023/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
